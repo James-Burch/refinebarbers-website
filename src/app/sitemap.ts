79dc03d843f1112185b49e490f0fbb017fc04182
James-Burch <burchjames6@gmail.com>
2025-08-28 03:38:12 +0100

    Push changes to sitemap file

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://refinebarbers-website.vercel.app'; // Update with real domain
+  const baseUrl = 'https://refinebarbers-website.vercel.app';
 
   return [
     {
